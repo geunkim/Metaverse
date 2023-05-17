486548be4c1122d9f08c4c3da21fd00867de21dd
Yoosung-H <101240036+Hongyoosung@users.noreply.github.com>
2023-05-17 16:25:12 +0900

    Add files via upload

---
+++ Project/Scripts/SceneTransition.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            SceneManager.LoadScene("Inside");
+        }
+    }
+}
