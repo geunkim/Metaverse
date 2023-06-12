7ea111aceb30edb5907bea63702e4884c6b5d6ed
portalchu <56888404+portalchu@users.noreply.github.com>
2023-06-12 18:37:40 +0900

    20230612_acapy코드테스트-및-unityHttp기능구현

--- HyperledgerAries/AvataDID/HttpClient.cs
+++ HyperledgerAries/AvataDID/HttpClient.cs
@@ -1,10 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 public class HttpClient : MonoBehaviour
 {
+
+    string test_url = "http://220.68.5.139:9000/genesis";
+    string acapy_url = "http://220.68.5.139:8001/connections";
+    string acapy_did_url = "http://220.68.5.139:8001/wallet/did";
+    string acapy_connection_url = "http://220.68.5.139:8001/connections/create-invitation";
+    string data = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +26,43 @@ public class HttpClient : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.L))
         {
             Debug.Log("HTTP TEST CHECK");
-            StartCoroutine(HttpTest());
+            StartCoroutine(HttpGet(test_url, (www) =>
+            {
+                data = www.downloadHandler.text;
+            }));
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("ACA-PY TEST CHECK");
+            StartCoroutine(HttpGet(acapy_did_url, (www) =>
+            {
+                data = www.downloadHandler.text;
+            }));
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("ACA-PY TEST CHECK POST");
+
+            string json = "{}";
+
+            StartCoroutine(HttpPost(acapy_connection_url, json, (www) =>
+            {
+                data = www.downloadHandler.text;
+            }));
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("data Check");
+            Debug.Log("data : " + data);
         }
     }
 
-    IEnumerator HttpTest() {
-        WWWForm form = new WWWForm();
-        using (UnityWebRequest www = UnityWebRequest.Get("http://220.68.5.139:9000/genesis"))
+    IEnumerator HttpGet(string url, Action<UnityWebRequest> callback) {
+
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
         {
             yield return www.SendWebRequest();
 
@@ -36,6 +75,32 @@ public class HttpClient : MonoBehaviour
                 Debug.Log(www.downloadHandler.text);
                 Debug.Log("Complete");
             }
+            www.Dispose();
+        }
+    }
+
+    IEnumerator HttpPost(string url, string json, Action<UnityWebRequest> callback)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(url, json))
+        {
+            byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+            www.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            //www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            //www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                Debug.Log("Complete");
+                callback(www);
+            }
+            www.Dispose();
         }
     }
 }
