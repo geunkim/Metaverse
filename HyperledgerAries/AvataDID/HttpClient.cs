2752e0ab880290c48b81d49f9d14aa1794564696
portalchu <56888404+portalchu@users.noreply.github.com>
2023-06-12 23:13:56 +0900

    Update HttpClient.cs

--- HyperledgerAries/AvataDID/HttpClient.cs
+++ HyperledgerAries/AvataDID/HttpClient.cs
@@ -8,7 +8,7 @@ using UnityEngine.Networking;
 public class HttpClient : MonoBehaviour
 {
 
-    string test_url = "http://220.68.5.139:9000/genesis";
+    string test_url = "http://220.68.5.139:9000/genesis"; // genesis file 링크
     string acapy_url = "http://220.68.5.139:8001/connections";
     string acapy_did_url = "http://220.68.5.139:8001/wallet/did";
     string acapy_connection_url = "http://220.68.5.139:8001/connections/create-invitation";
@@ -60,8 +60,15 @@ public class HttpClient : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// HTTP GET 사용 함수
+    /// </summary>
+    /// <param name="url">HTTP GET 요청을 보낼 URL</param>
+    /// <param name="callback">GET 연결 이후 실행할 Callback 함수</param>
+    /// <returns></returns>
     IEnumerator HttpGet(string url, Action<UnityWebRequest> callback) {
 
+        // HTTP 연결을 도와주는 UnityWebRequest 사용
         using (UnityWebRequest www = UnityWebRequest.Get(url))
         {
             yield return www.SendWebRequest();
@@ -79,6 +86,13 @@ public class HttpClient : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// HTTP POST 사용 함수
+    /// </summary>
+    /// <param name="url">HTTP POST 요청을 보낼 URL</param>
+    /// <param name="json">HTTP POST 요청시 보낼 값</param>
+    /// <param name="callback">POST 연결 이후 실행할 Callback 함수</param>
+    /// <returns></returns>
     IEnumerator HttpPost(string url, string json, Action<UnityWebRequest> callback)
     {
         using (UnityWebRequest www = UnityWebRequest.Post(url, json))
